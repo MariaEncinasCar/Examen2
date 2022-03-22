1fcb0ea5a1ae462e09a260558558a4402cde27c6
FidelBernal <75335307+FidelBernal@users.noreply.github.com>
2022-03-21 20:50:03 -0700

    intento de carrito
    no funciona

--- app/src/main/java/equipo3/examen2/carritoActivity.kt
+++ app/src/main/java/equipo3/examen2/carritoActivity.kt
@@ -2,10 +2,35 @@ package equipo3.examen2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.BaseAdapter
+import android.widget.ListView
 
 class carritoActivity : AppCompatActivity() {
+
+        var carrito = ArrayList<Detalles1>()
+        var carrito2 = ArrayList<Detalles2>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_carrito)
+
+        agregarPedido()
+        var adapter = Detalles1(this, pedido)
+        var adapter = Detalles2(this, pedido)
+        var listView: ListView = findViewById(R.id.listProductos)
+        listview.adapter = adapter
+
     }
+
+
+    private class Detalles1: BaseAdapter {
+        var pedido = ArrayList()
+
+    }
+
+    private class Detalles2: BaseAdapter {
+        var pedido = ArrayList()
+
+    }
+
 }
